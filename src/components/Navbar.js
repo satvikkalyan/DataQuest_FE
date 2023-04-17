796d5f364d93a20ad56f785d7541ab3910134ac6
sgundu <sgundu@iu.edu>
2023-04-16 22:04:52 -0400

    Side Bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { SidebarData } from "./SidebarData";
+import "../App.css";
+import { IconContext } from "react-icons";
+import "./navbar.css"
+function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: "undefined" }}>
+        <div className="navbar">
+          <Link to="#" className="menu-bars">
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+          <div>
+          <Link to="/about-us" className="menu-bar-link">
+              Data Quest
+            </Link>
+          </div>
+          <div className="menu-bar-right">
+
+            <Link to="/about-us" className="menu-bar-link">
+              Home
+            </Link>
+            <Link to="/about-us" className="menu-bar-link">
+              About Us
+            </Link>
+            <Link to="/logout" className="menu-bar-link">
+              Contact Us
+            </Link>
+            <Link to="/logout" className="menu-bar-link">
+              Logout
+            </Link>
+          </div>
+        </div>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            <li className="navbar-toggle">
+              <Link to="#" className="menu-bars">
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/components/SidebarData.js
@@ -0,0 +1,41 @@
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import SearchIcon from "@mui/icons-material/Search";
+import SettingsIcon from "@mui/icons-material/Settings";
+import LogoutIcon from "@mui/icons-material/Logout";
+import WorkIcon from "@mui/icons-material/Work";
+
+export const SidebarData = [
+  {
+    title: "Profile",
+    path: "/",
+    icon: <PermIdentityIcon />,
+    cName: "nav-text",
+  },
+  {
+    title: "Search",
+    path: "/reports",
+    icon: <SearchIcon />,
+    cName: "nav-text",
+  },
+  {
+    title: "Jobs",
+    path: "/products",
+    icon: <WorkIcon />,
+    cName: "nav-text",
+  },
+  {
+    title: "Settings",
+    path: "/team",
+    icon: <SettingsIcon />,
+    cName: "nav-text",
+  },
+  {
+    title: "Logout",
+    path: "/messages",
+    icon: <LogoutIcon />,
+    cName: "nav-text",
+  },
+];
