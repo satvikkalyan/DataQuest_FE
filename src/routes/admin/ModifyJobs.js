0711c51babbc4849dfa38d5ac04aef6141723d91
sgundu <sgundu@iu.edu>
2023-04-19 16:24:13 -0400

    Added Modify jobs Page

--- src/routes/admin/ModifyJobs.js
+++ src/routes/admin/ModifyJobs.js
@@ -2,9 +2,17 @@ import React from "react";
 import "./modifyJobs.css";
 import { TextField } from "@mui/material";
 import Button from "@mui/material/Button";
-import ControlPointIcon from "@mui/icons-material/ControlPoint";
 import BasicTable from "../../components/TableComponent";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+
 function ModifyJobs() {
+  const [payType, setPayType] = React.useState("Pay Type");
+
+  const handleChange = (event) => {
+    setPayType(event.target.value);
+  };
+
   return (
     <>
       <div className="search-container">
@@ -15,59 +23,55 @@ function ModifyJobs() {
         <div className="search-container-top">
           <div className="search-container-top-left">
             <p className="skills-title">Job Details:</p>
-          </div>
-          <div className="search-container-top-right">
-            <div className="Boxer">
-              <TextField
-                id="outlined-basic-1"
-                label="Job ID"
-                variant="outlined"
-              />
-            </div>
-            <div className="Boxer">
-              <TextField
-                id="outlined-basic-1"
-                label="Job Name"
-                variant="outlined"
-              />
-            </div>
-            <div className="Boxer">
-              <TextField
-                id="outlined-basic-1"
-                label="Job Name"
-                variant="outlined"
-              />
-            </div>
-            <div className="Boxer">
-              <TextField
-                id="outlined-basic-1"
-                label="Job Name"
-                variant="outlined"
-              />
-            </div>
-            <div className="Boxer">
-              <TextField
-                id="outlined-basic-1"
-                label="Job Name"
-                variant="outlined"
-              />
-            </div>
-            <div className="Boxer">
-              <TextField
-                id="outlined-basic-1"
-                label="Job Name"
-                variant="outlined"
-              />
-            </div>
-            <div className="Boxer plus-icon">
-              <ControlPointIcon />
-            </div>
-            <div className="Boxer plus-icon">
-              <Button variant="contained" className="Search-button">
-                Search
-              </Button>
+            <div className="buttons-container">
+              <div className="Boxer">
+                <TextField
+                  id="outlined-basic-1"
+                  label="Job Name"
+                  variant="outlined"
+                />
+              </div>
+              <div className="Boxer">
+                <TextField
+                  id="outlined-basic-1"
+                  label="Job Description"
+                  variant="outlined"
+                />
+              </div>
+              <div className="Boxer">
+                <TextField
+                  id="outlined-basic-1"
+                  label="Salary"
+                  variant="outlined"
+                />
+              </div>
+              <div className="Boxer">
+                <Select
+                  id="demo-simple-select"
+                  value={payType}
+                  label="Sassd"
+                  onChange={handleChange}
+                  sx={{ minWidth: "100%" }}
+                >
+                  <MenuItem value={1}>Per Hour</MenuItem>
+                  <MenuItem value={0}>Year</MenuItem>
+                </Select>
+              </div>
+              <div className="Boxer">
+                <TextField
+                  id="outlined-basic-1"
+                  label="Company"
+                  variant="outlined"
+                />
+              </div>
+              <div className="Boxer plus-icon">
+                <Button variant="contained" className="add-button">
+                  Add Job
+                </Button>
+              </div>
             </div>
           </div>
+          <div className="search-container-top-right"></div>
         </div>
         <div className="search-container-bottom">
           <div className="table-container">
