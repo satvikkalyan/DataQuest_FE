35e22c9eb1def60fd17d78d3b0ca46f7d5e15ef2
sgundu <sgundu@iu.edu>
2023-04-22 02:00:40 -0400

    Justified Text

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -3,7 +3,9 @@ import "./home.css";
 function Home() {
   const title_1 = "Welcome to DataQuest:";
   const description_1 =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum. Tincidunt dui ut ornare lectus. Sollicitudin ac orci phasellus egestas. Convallis aenean et tortor at risus viverra adipiscing. Feugiat vivamus at augue eget arcu dictum. Pulvinar elementum integer enim neque. Semper eget duis at tellus at urna. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Senectus et netus et malesuada fames ac.  ";
+    "DataQuest is an intuitive job search platform designed to assist job seekers in finding job opportunities that match their skillset. It identifies relevant job openings that are suitable for job seekers based on their current skills. Even if a job seeker lacks some of the required skills, DataQuest provides a comprehensive list of additional skills that need to be learned in order to qualify for the job.";
+  const description_1_2 =
+    "In addition to its advanced job matching capabilities, DataQuest also provides valuable insights into the job market through user-friendly charts and graphs. By presenting the most up-to-date job market data, job seekers can make informed decisions about their career path and identify potential growth areas. With its user-friendly interface and comprehensive data analysis, DataQuest is the ultimate job search tool for the modern job seeker.";
   const title_2 = "How to Use DataQuest:";
   const description_2 =
     "Id porta nibh venenatis cras. Orci porta non pulvinar neque laoreet. Congue quisque egestas diam in arcu cursus euismod quis. Volutpat ac tincidunt vitae semper. Euismod nisi porta lorem mollis. Pulvinar elementum integer enim neque. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique. Enim lobortis scelerisque fermentum dui faucibus. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique.";
@@ -15,18 +17,46 @@ function Home() {
       </div>
       <div className="home-content">
         <div className="home-container-left">
-        <div className="home-content-title content-sepatator">{title_1}</div>
-        <div className="home-content-description content-sepatator">
-          {description_1}
+          <div className="home-content-title content-sepatator">{title_1}</div>
+          <div className="home-content-description content-sepatator">
+            {description_1}
+            <br /> <br />
+            {description_1_2}
+          </div>
+          <div className="home-content-title content-sepatator">{title_2}</div>
+          <div className="home-content-description content-sepatator">
+            <div className="padder">
+              <ol>
+                <li>
+                  Create an account using the registration page by providing all
+                  the required details.
+                </li>
+                <li>Login to your account.</li>
+                <li>
+                  Use the search feature to find job openings that match your
+                  skills.
+                </li>
+                <li>
+                  If you are missing some of the required skills for a
+                  particular job, DataQuest will provide you with a list of
+                  additional skills that you need to learn.
+                </li>
+                <li>
+                  Use DataQuest charts and graphs to stay up-to-date on job
+                  market trends and make informed decisions about your career
+                  path.
+                </li>
+              </ol>
+              <br></br>
+              <p>
+                By following these steps, you can use DataQuest to find job
+                opportunities that match your skills and make informed decisions
+                about your career path.
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="home-content-title content-sepatator">{title_2}</div>
-        <div className="home-content-description content-sepatator">
-          {description_2}
-        </div>
-        </div>
-        <div className="home-container-right">
-        </div>
-        
+        <div className="home-container-right"></div>
       </div>
     </div>
   );
